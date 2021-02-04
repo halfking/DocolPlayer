d7354ecae6746dee3f34faf8f0a3f03de677c8d2
XUTAO HUANG <xutaohuang@XUTAOdeMacBook-Pro.local>
2021-02-04 15:45:15 +0800

    init

---
+++ DocolPlayer/DocolPlayer.h
@@ -0,0 +1,18 @@
+//
+//  DocolPlayer.h
+//  DocolPlayer
+//
+//  Created by XUTAO HUANG on 2021/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocolPlayer.
+FOUNDATION_EXPORT double DocolPlayerVersionNumber;
+
+//! Project version string for DocolPlayer.
+FOUNDATION_EXPORT const unsigned char DocolPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocolPlayer/PublicHeader.h>
+
+
